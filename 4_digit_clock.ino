7f61840626b6c10ccb6f38a924a102e84e8898a9
arulh <104797653+arulh@users.noreply.github.com>
2024-03-13 22:41:49 -0700

    Add files via upload

---
+++ 4_digit_clock.ino
@@ -0,0 +1,42 @@
+int pinA = 2;
+int pinB = 3;
+int pinC  = 4;
+int pinD = 5;
+int pinE = 6;
+int pinF = 7;
+int pinG = 8;
+int D1 = 9;
+int D2 = 10;
+int D3 = 11;
+int D4 = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pinA, OUTPUT);     
+  pinMode(pinB,  OUTPUT);     
+  pinMode(pinC, OUTPUT);     
+  pinMode(pinD, OUTPUT);     
+  pinMode(pinE, OUTPUT);     
+  pinMode(pinF, OUTPUT);     
+  pinMode(pinG,  OUTPUT);   
+  pinMode(D1, OUTPUT);  
+  pinMode(D2, OUTPUT);  
+  pinMode(D3,  OUTPUT);  
+  pinMode(D4, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2,  HIGH);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, LOW); 
+  //0
+  digitalWrite(pinA,  HIGH);   
+  digitalWrite(pinB, HIGH);   
+  digitalWrite(pinC, HIGH);   
+  digitalWrite(pinD, HIGH);   
+  digitalWrite(pinE, HIGH);   
+  digitalWrite(pinF,  HIGH);   
+  digitalWrite(pinG, LOW);
+}
