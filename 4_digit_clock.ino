2ad0f92bbd484bda16d34da79ae5d471bba10425
arulh <arul.howard@telus.net>
2024-04-07 15:25:10 -0700

    completed display functionality

--- 4_digit_clock.ino
+++ 4_digit_clock.ino
@@ -10,6 +10,131 @@ int D2 = 10;
 int D3 = 11;
 int D4 = 12;
 
+void one(){
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
+
+void two(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);
+}
+  
+ void three(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, HIGH);
+}
+    
+void four(){
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+    }
+    
+void five(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+    
+void six(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+    
+void seven(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+}
+    
+void eight(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, HIGH);
+  digitalWrite(pinE, HIGH);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+    
+void nine(){
+  digitalWrite(pinA, HIGH);
+  digitalWrite(pinB, HIGH);
+  digitalWrite(pinC, HIGH);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinG, HIGH);
+}
+
+void displayDigit(int mode) {
+   switch (mode) {
+      case 0: // reset
+        digitalWrite(D1, HIGH);
+        digitalWrite(D2,  HIGH);
+        digitalWrite(D3, HIGH);
+        digitalWrite(D4, HIGH);
+        break;
+      case 1:
+        digitalWrite(D1, LOW);
+        digitalWrite(D2,  HIGH);
+        digitalWrite(D3, HIGH);
+        digitalWrite(D4, HIGH);
+        break;
+      case 2:
+        digitalWrite(D1, HIGH);
+        digitalWrite(D2,  LOW);
+        digitalWrite(D3, HIGH);
+        digitalWrite(D4, HIGH);
+        break;
+      case 3:
+        digitalWrite(D1, HIGH);
+        digitalWrite(D2,  HIGH);
+        digitalWrite(D3, LOW);
+        digitalWrite(D4, HIGH);
+        break;
+      case 4:
+        digitalWrite(D1, HIGH);
+        digitalWrite(D2,  HIGH);
+        digitalWrite(D3, HIGH);
+        digitalWrite(D4, LOW);
+        break;
+   }
+}
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(pinA, OUTPUT);     
@@ -23,20 +148,33 @@ void setup() {
   pinMode(D2, OUTPUT);  
   pinMode(D3,  OUTPUT);  
   pinMode(D4, OUTPUT);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(D1, HIGH);
-  digitalWrite(D2,  HIGH);
-  digitalWrite(D3, LOW);
-  digitalWrite(D4, LOW); 
-  //0
-  digitalWrite(pinA,  HIGH);   
-  digitalWrite(pinB, HIGH);   
-  digitalWrite(pinC, HIGH);   
-  digitalWrite(pinD, HIGH);   
-  digitalWrite(pinE, HIGH);   
-  digitalWrite(pinF,  HIGH);   
-  digitalWrite(pinG, LOW);
+  if (Serial.available() > 0) {
+    int b = Serial.parseInt();
+
+    if (b == 1) {
+       while (1) {
+          displayDigit(3);
+          seven();
+          delay(5);
+
+          displayDigit(4);
+          four();
+          delay(5);
+
+          displayDigit(1);
+          one();
+          delay(5);
+
+          displayDigit(2);
+          two();
+          delay(5);
+       }
+    }
+  }
 }
